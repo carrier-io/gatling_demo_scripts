7d8991c9297b275e067a2bb3d3c777b69c8a0c72
Mykhailo_Hunko <mykhailo.hunko@gmail.com>
2023-12-01 11:52:02 +0200

    add demo simulation with groups

---
+++ user-files/simulations/carrier/FloodIoJavaWithGroups.java
@@ -0,0 +1,54 @@
+package carrier;
+
+import io.gatling.javaapi.core.Choice;
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.http.HttpProtocolBuilder;
+
+import static carrier.requestsjava.FloodIORequestsJava.*;
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.http;
+
+public class FloodIoJavaWithGroups extends Simulation {
+
+    String environment = "https://training.flooded.io";
+    //String environment = System.getProperty("apiUrl");
+    //String environment = System.getProperty("environment");
+    //String environment = System.getenv("demo_environment");
+    //String environment = "https://training.flooded.io";
+    //   int ramp_users = 5;
+    int ramp_users = Integer.getInteger("ramp_users", 5);
+    //   int ramp_duration = 120;
+    int ramp_duration = Integer.getInteger("ramp_duration", 30);
+    //   int duration = 120;
+    int duration = Integer.getInteger("duration", 60);
+
+
+    HttpProtocolBuilder webProtocol = http
+            .baseUrl(environment)
+            .disableCaching()
+            .disableFollowRedirect();
+
+    ScenarioBuilder flood_io = scenario("flood_io")
+            .during(duration).on(
+                    group("first_group").on(
+                                    exec(Step1GET).exec(Step1POST)
+                            )
+                            .group("second_group").on(
+                                    exec(Step2GET).exec(Step2POST)
+                            )
+                            .exec(Step3GET)
+                            .exec(Step3POST)
+                            .exec(Step4GET)
+                            .exec(Step4POST)
+                            .exec(dataJSON)
+                            .exec(Step5GET)
+                            .exec(Step5POST)
+                            .randomSwitch()
+                            .on(Choice.withWeight(60.0, FinalStep), Choice.withWeight(40.0, failedFinalStep)));
+
+
+    {
+        setUp(flood_io.injectOpen(rampUsers(ramp_users).during(ramp_duration)).protocols(webProtocol));
+    }
+}
